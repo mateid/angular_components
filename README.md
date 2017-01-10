4dc1cdb6a754c43ba84f6c5f6f0659731f65a753
Matei Dorobantu <mateid@aedigital.com>
2017-01-10 15:31:35 -0700

    Updated to launch easily and illustrate the transition issues I've been having.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Angular components
-Just a demo of using angular material with angular components and ui-router.
+
+Just a demo of using angular material with angular components and ui-router. Run `npm start` to launch.
--- sample.js
+++ sample.js
@@ -1,44 +1,50 @@
 var myApp = angular.module('sample', ['ngMaterial', 'ui.router', 'ngAnimate']);
 
 myApp
-  .config(function($mdThemingProvider){
+  .config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default');
   })
-  .component('hello', {
-    template:  '<h3>{{$ctrl.greeting}} Solar System!</h3>',
-    controller: function() {
-      this.greeting = 'Hello';
-      this.toggleGreeting = function() {
-        this.greeting = (this.greeting == 'hello') ? 'whats up' : 'hello';
-      }
-    }
-  })
+  .run(['$trace', function ($trace) {
+    $trace.enable('TRANSITION');
+  }])
+  .run(['$transitions', function ($transitions) {
+    $transitions.onStart({}, function (transition) {
+      console.log('Starting transition!');
+      transition.promise.finally(console.log('Transition promise finally fired!'));
+    });
+    $transitions.onError({}, function (transition) {
+      console.log('Transition erred!');
+    });
+  }])
   .component('toolbar', {
     templateUrl: 'toolbar.tmpl.html',
-    controller: function($state) {
+    controller: function ($state) {
       this.name = 'controller!';
-      this.navigate = function(routeName) {
+      this.navigate = function (routeName) {
         $stateProvider.go(routeName);
       }
     }
   })
-  .component('repeater', {
-    templateUrl: 'repeater.tmpl.html',
-    controller: function() {
-      this.items = ['A repeater item','One more item','The last item'];
-      this.key = this.items.length;
-      this.addItem = function() {
-        this.items.push('Another repeater item (' + this.key + ')');
-        this.key++;
-      };
-      this.removeItem = function(item) {
-        var index = this.items.indexOf(item);
-        this.items.splice(index, 1);
-      };
+  .component('hello', {
+    template: '<h3>{{$ctrl.greeting}} Solar System!</h3>',
+    controller: function () {
+      this.greeting = 'Hello';
+      this.toggleGreeting = function () {
+        this.greeting = (this.greeting == 'hello') ? 'whats up' : 'hello';
+      }
+    }
+  })
+  .component('about', {
+    template: '<h3>Demo of transitions behaving strangely</h3>',
+    controller: function ($state) {
+      this.name = 'controller!';
+      this.navigate = function (routeName) {
+        $stateProvider.go(routeName);
+      }
     }
   });
 
-myApp.config(function($stateProvider) {
+myApp.config(function ($stateProvider) {
   $stateProvider.state({
     name: 'hello',
     url: '/hello',
@@ -46,6 +52,6 @@ myApp.config(function($stateProvider) {
   }).state({
     name: 'about',
     url: '/about',
-    component: 'repeater'
+    component: 'about'
   });
 });
